4dc63144d0eb1bf22e0e180495e5ccad63fc6859
Vincent Riesop <v.riesop@sap.com>
2023-06-19 17:28:00 +0200

    wip: build package. still needs fixing permission issue.

--- package_builder/build_package.py
+++ package_builder/build_package.py
@@ -14,21 +14,53 @@ arch_dict = {
     'arm64': 'aarch64-linux-gnu',
 }
 
+
+
 def build_package(source_name, source_dist, arch, workdir):
     overwrite_debian = True
     orig_tar = False 
 
     try:
         install_build_dependencies(workdir, arch)
+        extract_source(workdir)
+        do_build(f"{workdir}/src", arch)
     except subprocess.CalledProcessError as e:
         sc.logger.error(f"Subprocess failed:\n{e.stderr.decode()}")
         sc.list_directory_contents(workdir)
+
     except Exception as e:
         #spawn_bash()
         sc.logger.error("Caught Exception. Cleaning up now.")
         sc.logger.error(e)
 
         #delete_folder(output_dir)
+
+def extract_source(workdir):
+    dsc_file = get_dsc_file(workdir)
+    command = ['dpkg-source', '-x', dsc_file, f"{workdir}/src"]
+    subprocess.run(command, cwd=workdir,  stderr=subprocess.PIPE, check=True)
+
+
+def do_build(workdir, target_arch):
+    dpkg_arch = subprocess.run(['dpkg', '--print-architecture'], capture_output=True, text=True).stdout.strip()
+
+    if target_arch != dpkg_arch:
+        if "DEB_BUILD_OPTIONS" not in os.environ:
+            os.environ["DEB_BUILD_OPTIONS"] = "nocheck"
+        else:
+            os.environ["DEB_BUILD_OPTIONS"] += " nocheck"
+
+        if "DEB_BUILD_PROFILES" not in os.environ:
+            os.environ["DEB_BUILD_PROFILES"] = "cross"
+        else:
+            os.environ["DEB_BUILD_PROFILES"] += " cross"
+
+    if target_arch == 'all':
+        subprocess.run(["su", "-s", "/bin/sh", "-c", f"set -euE; dpkg-buildpackage -A", "nobody"], cwd=workdir, check=True)
+    else:
+        subprocess.run(["su", "-s", "/bin/sh", "-c", f"set -euE; dpkg-buildpackage -B -a {target_arch}", "nobody"], cwd=workdir, check=True)
+
+
 
 def get_dsc_file(directory_path):
     files = glob.glob(os.path.join(directory_path, "*.dsc"))
@@ -49,7 +81,7 @@ def install_build_dependencies(workdir, target_arch, deb_build_profiles=None):
         subprocess.run(["apt-get", "build-dep", "-qy", "--indep-only", "-o", "DPkg::Options::=--force-unsafe-io", dsc_file], cwd=workdir, stderr=subprocess.PIPE, check=True)
     else:
         gnu_type = arch_dict[target_arch]
-        dpkg_arch = subprocess.run(["dpkg", "--print-architecture"], capture_output=True, text=True,  stderr=subprocess.PIPE, check=True).stdout.strip()
+        dpkg_arch = subprocess.run(["dpkg", "--print-architecture"], capture_output=True, text=True).stdout.strip()
         if target_arch != dpkg_arch:
             if "DEB_BUILD_PROFILES" not in os.environ:
                 os.environ["DEB_BUILD_PROFILES"] = "cross"
